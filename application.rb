66824d1fb5860a5d6cbb2040aebed16e7dfebf8b
Jim Patterson <jpatterson@yammer-inc.com>
2010-06-14 15:38:58 -0700

    Add /env route

--- application.rb
+++ application.rb
@@ -11,3 +11,13 @@ end
 get '/' do
   erb :index
 end
+
+# Test at <appname>.heroku.com
+
+# You can see all your app specific information this way.
+# IMPORTANT! This is a very bad thing to do for a production
+# application with sensitive information
+
+get '/env' do
+  ENV.inspect
+end
